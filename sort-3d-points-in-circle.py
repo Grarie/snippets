14c15b75b7c9d0ae13f1e19d7666bf7fead9ca3d
Grarie <sebastu91@yahoo.de>
2022-02-20 15:48:27 +0100

    vector sorting: improved readability

--- sort-3d-points-in-circle.py
+++ sort-3d-points-in-circle.py
@@ -4,26 +4,38 @@ import matplotlib.pyplot as plt
 
 np.random.seed(19680801)
 
-def vectors_sine(A, B, C, n):
+def normalized(a, axis=-1, order=2):
+	# https://stackoverflow.com/questions/21030391/how-to-normalize-a-numpy-array-to-a-unit-vector
+    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+    l2[l2==0] = 1
+    return a / np.expand_dims(l2, axis)
+
+
+def vectors_sine_sign(a, B, c, n):
 	# returns sine of the angle between the 3D vectors CA and CB with
 	# n beeing the normal pointing "up"
 	# there is no check, that the points are coplanar and that
 	# n is indeed the normal to the spanned plane
-    return np.dot(n, np.cross(A - C, B - C))
+	# We don't normalize the vectors, as the *actual* sine is not needed, just the sign
+    return np.dot(np.cross(a - c, B - c), n)
+
+def cosine_with(A,b):
+	return np.dot( normalized(A), normalized(b)[0] )
 
 def angular_pnt_sort(points, axis=np.array([0,0,0]), normal=np.array([0, 0, 1]), pivot=0):
 	# sorts the given coplanar points in a circle
 	# axis is the center around which to sort, pivot is a reference vector, normal is the normal to the plane spanned by the points
 	# use sine to divide the points in 180° before and after
-	angls = np.array([vectors_sine(points[pivot], pnt, axis, normal) for pnt in points])
+	angls = vectors_sine_sign(points[pivot], points, axis, normal)
 	before = points[ 0>angls ]
 	after = points[ 0<=angls ]
 	# now we can use the cosinus-values to sort 
-	cosins_before = np.dot( [normalize(pnt) for pnt in (before-axis)], [normalize(pnt) for pnt in (points[pivot]-axis)] )
-	cosins_after = np.dot( [normalize(pnt) for pnt in (after-axis)], [normalize(pnt) for pnt in (points[pivot]-axis)])
+	cosins_before = cosine_with(before-axis, points[pivot]-axis)
+	cosins_after = cosine_with(after-axis, points[pivot]-axis)
 	before = before[np.argsort(cosins_before)]
 	after = after[np.argsort(-cosins_after)]
 	return np.concatenate( (before,	after) )
+
 
 # slow resursive method with too much "array creation"
 # first try as a proof of concept
@@ -48,6 +60,7 @@ def sorted(points, axis=np.array([0,0,0]), normal=np.array([0, 0, 1]), pivot=0):
 			))
 	else:
 		return points
+
 
 def normalize(pnt):
 	norm = np.sqrt((pnt**2).sum())
